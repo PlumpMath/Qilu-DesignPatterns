16324d3e5af894a168e24c42b2cefab89515871a
xieqilu <xieqilu1989@gmail.com>
2015-04-23 01:07:43 -0700

    Update 3.AdapterPattern.cs

--- 3.AdapterPattern.cs
+++ 3.AdapterPattern.cs
@@ -1,3 +1,25 @@
+/**
+Adapter Pattern
+
+What is Adapter Pattern
+Adapter pattern acts as a bridge between two incompatible interfaces. This pattern involves a single class called adapter 
+which is responsible for communication between two independent or incompatible interfaces.
+For Example: 
+A card reader acts as an adapter between memory card and a laptop. 
+You plugins the memory card into card reader and card reader into the laptop so that memory card can be read via laptop.
+
+The classes, interfaces and objects in this pattern are as follows:
+01.ITarget This is an interface which is used by the client to achieve its functionality/request.
+
+02.Adapter This is a class which implements the ITarget interface and inherits the Adaptee class. 
+It is responsible for communication between Client and Adaptee.
+
+03.Adaptee This is a class which have the functionality, required by the client. 
+However, its interface is not compatible with the client.
+
+04.Client This is a class which interact with a type that implements 
+*/
+
 //Simple Example for Adapter pattern
 
 using System;
